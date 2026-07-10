009c32244cf260323727c08c2945133753096d74
Mabel <ezemamabel57@gmail.com>
2026-07-10 21:33:51 +0100

    fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo({ size = 52 }: { size?: number }) {
     return (
         <Image
-            src="/nesi-logo-navy.png"
+            src="/nesi-logo-navy.jpeg"
             alt="NESI Week logo"
             width={size}
             height={size}
